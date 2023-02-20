cfabf56d8a808ca65d53c22ec5d7e9bde937d58b
rChrisb <5767@holbertonstudents.com>
2023-02-20 11:47:31 +0100

    Write a class BaseModel that defines all common attributes/methods for other classes:

---
+++ models/base_model.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""base model"""
+
+import uuid
+from datetime import datetime
+
+
+class BaseModel:
+    """Class defining all common attributes/methods for other classes"""
+    def __init__(self, id=str(uuid.uuid4()), created_at=datetime.now(),
+                 updated_at=datetime.now()):
+        self.id = id
+        self.created_at = created_at
+        self.updated_at = updated_at
+
+    def __str__(self):
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+
+    def save(self):
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        dict = self.__dict__
+        dict["__class__"] = f"{self.__class__.__name__}"
+        dict["updated_at"] = self.updated_at.isoformat()
+        dict["created_at"] = self.created_at.isoformat()
+        return dict
