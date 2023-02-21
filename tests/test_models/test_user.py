49ee5cb615a9787d3549a0b63e4cf33405b93dcd
rChrisb <5767@holbertonstudents.com>
2023-02-21 11:25:50 +0100

    tests for user class

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -6,20 +6,18 @@ from models.user import User
 class TestUser(unittest.TestCase):
     def test_user_email(self):
         user = User()
-        user.email = "user@gmail.com"
-        self.assertNotEqual(user.email, User.email)
+        user.email = "someemail.com"
+        self.assertEqual(0, len(User.email))
 
     def test_user_password(self):
-        user = User()
-        user.password = "mypass"
-        self.assertNotEqual(user.password, User.password)
+        self.assertEqual("", User.password)
 
     def test_user_first_name(self):
         user = User()
         user.first_name = "victor"
-        self.assertNotEqual(user.first_name, User.first_name)
+        self.assertEqual("", User.first_name)
 
     def test_user_last_name(self):
         user = User()
         user.last_name = "Dubonus"
-        self.assertNotEqual(user.last_name, User.last_name)
+        self.assertEqual("", User.last_name)
